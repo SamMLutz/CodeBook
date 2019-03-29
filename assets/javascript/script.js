903421cfb4249d643981bb47ab3f02ab67fd4894
samlutz <sammlutz@gmail.com>
2019-03-28 20:25:56 -0400

    working functionality for news search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,15 +33,20 @@ $.ajax({
 
 $("#submit-button").on("click", function (event) {
   event.preventDefault();
+  
   var searchRequest = $("#inputField").val().trim();
   var allNewsQueryURL = "https://newsapi.org/v2/everything?q=" + searchRequest + "&apiKey=" + apiKey
-
+  $("#results").empty();
+  $("#inputField").val("");
   $.ajax({
     url: allNewsQueryURL,
     method: "GET"
   }).then(function (response) {
+    
     console.log(response, response.articles[1].title);
     for (i = 0; i < 5; i++) {
+      
+      
       var articleUrl = response.articles[i].url;
 
       var headlineLink = $("<a href=" + articleUrl + "></a>").text(response.articles[i].url)
@@ -49,7 +54,7 @@ $("#submit-button").on("click", function (event) {
       headlineLink.attr("target", "#");
 
       var articleTitle = $("<p>").text("Title: " + response.articles[i].title);
-      $("#searchDiv").append(articleTitle, headlineLink)
+      $("#results").append(articleTitle, headlineLink)
 
       console.log(response.articles[i].title);
       console.log(response.articles[i].url);
